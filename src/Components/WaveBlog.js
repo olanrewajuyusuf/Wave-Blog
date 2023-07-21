46216a50a2fca413345071700fc1114945974c33
olanrewajuyusuf <olanrewajuyusuf280@gmail.com>
2023-07-21 18:07:20 +0100

    Initial commit

---
+++ src/Components/WaveBlog.js
@@ -0,0 +1,11 @@
+
+const WaveBlog = () => {
+  return (
+    <div style={{margin: "0 auto", width: "100%"}}>
+        <h1>Welcome to WaveBlog</h1>
+        <p>Get ready to explore variety of news and blogs</p>
+    </div>
+  )
+}
+
+export default WaveBlog
